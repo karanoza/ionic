88ec988f0e3a57fc4531b22188809b80d98e1bcc
Karan Oza <50609276+karanoza@users.noreply.github.com>
2025-06-06 17:55:54 +0530

    Added offers and my bookings page logic

--- Ionic-with-Angular/ionic-booking-app/src/app/places/discover/place-detail/place-detail.page.ts
+++ Ionic-with-Angular/ionic-booking-app/src/app/places/discover/place-detail/place-detail.page.ts
@@ -1,20 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonBackButton, IonButton, NavController } from '@ionic/angular/standalone';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-place-detail',
   templateUrl: './place-detail.page.html',
   styleUrls: ['./place-detail.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonButton, IonBackButton, IonButtons, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
 })
 export class PlaceDetailPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router, private navCtrl: NavController) { }
 
   ngOnInit() {
   }
 
+onBookPlace(){
+  //this.router.navigateByUrl('/places/tabs/discover');
+  this.navCtrl.navigateBack('/places/tabs/discover');
+}
 }
--- Ionic-with-Angular/ionic-booking-app/src/app/places/offers/offers.page.ts
+++ Ionic-with-Angular/ionic-booking-app/src/app/places/offers/offers.page.ts
@@ -1,20 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonGrid, IonRow, IonCol, IonList, IonItem, IonImg, IonLabel } from '@ionic/angular/standalone';
+import { PlacesService } from '../places.service';
+import { Place } from '../place.model';
+import { IonicModule } from '@ionic/angular';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-offers',
   templateUrl: './offers.page.html',
   styleUrls: ['./offers.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [CommonModule, FormsModule, IonicModule, RouterModule]
 })
 export class OffersPage implements OnInit {
+  offers!: Place[];
 
-  constructor() { }
+  constructor(private placesService: PlacesService) { }
 
   ngOnInit() {
+    this.offers = this.placesService.places;
   }
 
 }
