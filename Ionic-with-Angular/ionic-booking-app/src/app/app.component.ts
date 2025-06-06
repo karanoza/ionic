60dd261a815aeb5777749886d3a94305edab5f7d
Karan Oza <50609276+karanoza@users.noreply.github.com>
2025-06-06 20:04:22 +0530

    menu dropdown added

--- Ionic-with-Angular/ionic-booking-app/src/app/app.component.ts
+++ Ionic-with-Angular/ionic-booking-app/src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
-import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import { IonicModule } from '@ionic/angular';
+import {
+  IonApp,
+  IonRouterOutlet,
+  IonMenu,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonList,
+  IonItem,
+  IonIcon,
+  IonLabel
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  imports: [IonApp, IonRouterOutlet],
+  imports: [IonApp, IonRouterOutlet, IonMenu,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonList,
+  IonItem,
+  IonIcon,
+  IonLabel],
 })
 export class AppComponent {
   constructor() {}
