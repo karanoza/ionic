b66f440b3a854a13ff9ffe96f5a9f25adfcd0e08
Karan Oza <50609276+karanoza@users.noreply.github.com>
2025-06-09 13:54:25 +0530

    Added actioncontroller

--- Ionic-with-Angular/ionic-booking-app/src/app/places/discover/place-detail/place-detail.page.ts
+++ Ionic-with-Angular/ionic-booking-app/src/app/places/discover/place-detail/place-detail.page.ts
@@ -1,7 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonBackButton, IonButton, NavController, ModalController } from '@ionic/angular/standalone';
+import {
+  IonContent,
+  IonHeader,
+  IonTitle,
+  IonToolbar,
+  IonButtons,
+  IonBackButton,
+  IonButton,
+  NavController,
+  ModalController,
+  ActionSheetController,
+} from '@ionic/angular/standalone';
 import { ActivatedRoute, Router } from '@angular/router';
 import { PlacesService } from '../../places.service';
 import { Place } from '../../place.model';
@@ -12,37 +23,86 @@ import { CreateBookingComponent } from 'src/app/bookings/create-booking/create-b
   templateUrl: './place-detail.page.html',
   styleUrls: ['./place-detail.page.scss'],
   standalone: true,
-  imports: [IonButton, IonBackButton, IonButtons, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [
+    IonButton,
+    IonBackButton,
+    IonButtons,
+    IonContent,
+    IonHeader,
+    IonTitle,
+    IonToolbar,
+    CommonModule,
+    FormsModule,
+  ],
 })
 export class PlaceDetailPage implements OnInit {
-  place!: Place
+  place!: Place;
 
-  constructor(private router: Router,private modalCtrl: ModalController,private route: ActivatedRoute, private navCtrl:NavController, private placesService:PlacesService) { }
+  constructor(
+    private router: Router,
+    private modalCtrl: ModalController,
+    private route: ActivatedRoute,
+    private navCtrl: NavController,
+    private actionSheetCtrl: ActionSheetController,
+    private placesService: PlacesService
+  ) {}
 
   ngOnInit() {
-     this.route.paramMap.subscribe(paramMap => {
-      if(!paramMap.has('placeId')){
+    this.route.paramMap.subscribe((paramMap) => {
+      if (!paramMap.has('placeId')) {
         this.navCtrl.navigateBack('places/tabs/discover');
         return;
       }
-      this.place = this.placesService.getPlace(paramMap.get('placeId'))
-  })
+      this.place = this.placesService.getPlace(paramMap.get('placeId'));
+    });
   }
 
-onBookPlace(){
-  //this.router.navigateByUrl('/places/tabs/discover');
-  this.navCtrl.navigateBack('/places/tabs/discover');
-  this.modalCtrl.create({
-    component: CreateBookingComponent,
-    componentProps: { selectedPlace: this.place }
-  }).then(modalEl => {
-    modalEl.present();
-    return modalEl.onDidDismiss();
-  }).then(resultData => {
-    console.log(resultData.data, resultData.role);
-    if(resultData.role === 'confirm'){
-      console.log('BOOKED')
-    }
-  })
-}
+  onBookPlace() {
+    //this.router.navigateByUrl('/places/tabs/discover');
+
+    this.actionSheetCtrl.create({
+      header: 'Choose an Action',
+      buttons: [
+        {
+          text: 'Select Date',
+          handler: () => {
+            this.openBookingModal('select');
+          },
+        },
+        {
+          text: 'Random Date',
+          handler: () => {
+            this.openBookingModal('random')
+          },
+        },
+        {
+          text: 'Cancel',
+          role: 'cancel',
+        },
+      ],
+    }).then(actionSheetEl => {
+      actionSheetEl.present()
+    });
+    this.navCtrl.navigateBack('/places/tabs/discover');
+    
+  }
+
+  openBookingModal(mode: 'select' | 'random'){
+    console.log(mode);
+    this.modalCtrl
+      .create({
+        component: CreateBookingComponent,
+        componentProps: { selectedPlace: this.place },
+      })
+      .then((modalEl) => {
+        modalEl.present();
+        return modalEl.onDidDismiss();
+      })
+      .then((resultData) => {
+        console.log(resultData.data, resultData.role);
+        if (resultData.role === 'confirm') {
+          console.log('BOOKED');
+        }
+      });
+  }
 }
