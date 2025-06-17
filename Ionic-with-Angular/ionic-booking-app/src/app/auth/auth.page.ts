456669d08d7e775513cb2e36658f71ed4436ff7f
Karan Oza <50609276+karanoza@users.noreply.github.com>
2025-06-17 23:45:50 +0530

    added authentication boiler plate

--- Ionic-with-Angular/ionic-booking-app/src/app/auth/auth.page.ts
+++ Ionic-with-Angular/ionic-booking-app/src/app/auth/auth.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, NgForm } from '@angular/forms';
 import { IonContent,IonInput,IonItem, IonLabel, IonHeader, IonTitle, IonToolbar, IonButton, IonList, IonSpinner, LoadingController, IonGrid, IonRow, IonCol } from '@ionic/angular/standalone';
 import { AuthService } from './auth.service';
 import { Router } from '@angular/router';
@@ -14,6 +14,7 @@ import { Router } from '@angular/router';
 })
 export class AuthPage implements OnInit {
   isLoading = false;
+  isLogin = true;
 
   constructor(private authService: AuthService, private loadingCtrl:LoadingController,private router: Router) { }
 
@@ -34,6 +35,26 @@ export class AuthPage implements OnInit {
     }, 1500)
     })  
     
+
+  
+  }
+  onSubmit(form: NgForm){
+     if(!form.valid){
+      return
+     }
+     const email = form.value.email;
+     const password = form.value.password;
+
+     console.log(email, password)
+
+     if(this.isLogin){
+      // Send a request to login server
+     } else {
+      // Send a request to signup server
+     }
   }
 
+  onSwitchAuthModel(){
+    this.isLogin = !this.isLogin;
+  }
 }
