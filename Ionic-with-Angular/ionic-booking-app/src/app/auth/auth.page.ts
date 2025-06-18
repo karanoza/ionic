5f044ea6bc8de114596e1ebc4ac33dd808ccf0e4
Karan Oza <50609276+karanoza@users.noreply.github.com>
2025-06-18 23:37:45 +0530

    minor change

--- Ionic-with-Angular/ionic-booking-app/src/app/auth/auth.page.ts
+++ Ionic-with-Angular/ionic-booking-app/src/app/auth/auth.page.ts
@@ -1,7 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, NgForm } from '@angular/forms';
-import { IonContent,IonInput,IonItem, IonLabel, IonHeader, IonTitle, IonToolbar, IonButton, IonList, IonSpinner, LoadingController, IonGrid, IonRow, IonCol } from '@ionic/angular/standalone';
+import {
+  IonContent,
+  IonInput,
+  IonItem,
+  IonLabel,
+  IonHeader,
+  IonTitle,
+  IonToolbar,
+  IonButton,
+  IonList,
+  IonSpinner,
+  LoadingController,
+  IonGrid,
+  IonRow,
+  IonCol,
+} from '@ionic/angular/standalone';
 import { AuthService } from './auth.service';
 import { Router } from '@angular/router';
 
@@ -10,51 +25,66 @@ import { Router } from '@angular/router';
   templateUrl: './auth.page.html',
   styleUrls: ['./auth.page.scss'],
   standalone: true,
-  imports: [IonButton,IonGrid,IonRow,IonCol,IonSpinner,IonList,IonItem,IonInput,IonLabel,IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [
+    IonButton,
+    IonGrid,
+    IonRow,
+    IonCol,
+    IonList,
+    IonItem,
+    IonInput,
+    IonLabel,
+    IonContent,
+    IonHeader,
+    IonTitle,
+    IonToolbar,
+    CommonModule,
+    FormsModule,
+  ],
 })
 export class AuthPage implements OnInit {
   isLoading = false;
   isLogin = true;
 
-  constructor(private authService: AuthService, private loadingCtrl:LoadingController,private router: Router) { }
+  constructor(
+    private authService: AuthService,
+    private loadingCtrl: LoadingController,
+    private router: Router
+  ) {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
-  login(){
+  login() {
     this.isLoading = true;
     this.authService.login();
     this.loadingCtrl
-    .create({keyboardClose:true, message: 'Logging in...'})
-    .then(loadingEl => {
-      loadingEl.present();
-      setTimeout(() =>{
-      this.isLoading = false;
-      loadingEl.dismiss();
-        this.router.navigateByUrl('/places/tabs/discover')
-    }, 1500)
-    })  
-    
-
-  
+      .create({ keyboardClose: true, message: 'Logging in...' })
+      .then((loadingEl) => {
+        loadingEl.present();
+        setTimeout(() => {
+          this.isLoading = false;
+          loadingEl.dismiss();
+          this.router.navigateByUrl('/places/tabs/discover');
+        }, 1500);
+      });
   }
-  onSubmit(form: NgForm){
-     if(!form.valid){
-      return
-     }
-     const email = form.value.email;
-     const password = form.value.password;
+  onSubmit(form: NgForm) {
+    if (!form.valid) {
+      return;
+    }
+    const email = form.value.email;
+    const password = form.value.password;
 
-     console.log(email, password)
+    console.log(email, password);
 
-     if(this.isLogin){
+    if (this.isLogin) {
       // Send a request to login server
-     } else {
+    } else {
       // Send a request to signup server
-     }
+    }
   }
 
-  onSwitchAuthModel(){
+  onSwitchAuthModel() {
     this.isLogin = !this.isLogin;
   }
 }
